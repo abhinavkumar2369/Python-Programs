8c9570b1d9f0df5c4827fa6f33e01c7a51b6b530
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-05 00:52:18 +0530

    ✅ Done

---
+++ String/Remove Vowel.py
@@ -0,0 +1,16 @@
+# Program to remove vowels from the string
+
+
+def remove_vowel(string):
+    new_string = ''
+    vowel = ['a','e','i','o','u']
+    for a in string:
+        if a.lower() in vowel:
+            continue
+        else:
+            new_string += a
+    return new_string
+
+
+a = 'Hello I am Shy Duck'
+print(remove_vowel(a))
