ce9e3e9c82f2e85f4ff9a16245b0ac0d585238ad
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-05 00:48:34 +0530

    ✅ Done

---
+++ List/Descending Order Lists.py
@@ -0,0 +1,8 @@
+# Program to arrange the element in the descending order in a list
+
+def descending_order(list):
+    list.reverse()
+    return list
+
+list = [10,3,4,16,1]
+print(descending_order(list))
