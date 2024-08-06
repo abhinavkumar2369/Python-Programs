769bdec450fc12538f457497b3d0d9766b04b1bf
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-07 00:39:52 +0530

    ✅ Correct

---
+++ Library/package/math.py
@@ -0,0 +1,18 @@
+# ----------- Sum -----------
+def add(a,b):
+    return a+b
+
+
+# --------- Subtract ---------
+def subtract(a,b):
+    return a-b
+
+
+# --------- Multiply ---------
+def multiply(a,b):
+    return a*b
+
+
+# --------- Divide -----------
+def divide(a,b):
+    return a/b
