5f90994e870d72714f94b93f70ed1d1c1020b655
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-06 20:34:18 +0530

    ✅ Done

---
+++ String/Output_1.py
@@ -0,0 +1,8 @@
+def count(s):
+    for str in string.split():
+        s = '&'.join(str)
+    return s
+
+print(count("Python is fun to learn."))
+
+#### --------------- Error in question --------------------
