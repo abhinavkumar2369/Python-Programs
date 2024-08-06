d5a1cb09bbec025144efad439bcb64b9267eade1
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-07 00:39:09 +0530

    Package Made ✏️

---
+++ Library/Method_2.py
@@ -0,0 +1,21 @@
+from package import math, english
+
+# ----------- Math Module ------------
+print(math.add(1,2))
+print(math.multiply(2,3))
+print(math.divide(3,4))
+print(math.subtract(4,2))
+
+
+# --------- English Module -----------
+english.name("Abhinav")
+
+
+
+# -------------- Output --------------
+
+# 3
+# 6
+# 0.75
+# 2
+# My name is Abhinav.
---
+++ Library/method_1.py
@@ -0,0 +1,9 @@
+from package.math import add
+from package.english import name
+
+
+print(add(1,2))
+# 3
+
+name("Abhinav")
+# My name is Abhinav
